9179b1caa90ff805174e251bac49750fc5b9db27
tyler.adams <tyler.adams@thalmic.com>
2015-02-22 03:05:52 -0500

    Add scariness editing to specific activities.

--- example/src/main/java/com/lorentzos/swipecards/ProfileUserActivity.java
+++ example/src/main/java/com/lorentzos/swipecards/ProfileUserActivity.java
@@ -29,6 +29,8 @@ public class ProfileUserActivity extends Activity {
     private ArrayList<Integer> swipeResults;
     private ArrayList<String> stringAdapterList;
     private ArrayList<String> nameList;
+    private ArrayList<Integer> nContributorsList;
+    private ArrayList<String> activityNameList;
     private ArrayAdapter<String> arrayAdapter;
     private Firebase firebaseRef;
     private String username;
@@ -48,6 +50,8 @@ public class ProfileUserActivity extends Activity {
         stringAdapterList = new ArrayList<String>();
         nameList = new ArrayList<String>();
         swipeResults = new ArrayList<Integer>();
+        nContributorsList = new ArrayList<Integer>();
+        activityNameList = new ArrayList<String>();
 
         // from the user's responses on the particular activities, compute personal threshold
         arrayAdapter = new ArrayAdapter<>(ProfileUserActivity.this, R.layout.item, R.id.helloText, stringAdapterList);
@@ -116,12 +120,19 @@ public class ProfileUserActivity extends Activity {
         scoresRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
-                //Log.d("Snapshot", ""+snapshot.getValue());
-                for(Object activity : ((Map<Object, Object>) snapshot.getValue()).values()) {
+                Log.d("Snapshot", ""+snapshot.getValue());
+
+                for(Object activityKey : ((Map<Object, Object>) snapshot.getValue()).keySet()) {
+                    Object activity =  ((Map<Object, Object>) snapshot.getValue()).get(activityKey);
+
+                   //Log.d("ProfileUserActivity", "activity name: "+activityKey);
+                    activityNameList.add(""+activityKey);
                     stringAdapterList.add(""+((Map<Object, Object>)activity).get("name"));
                     nameList.add(""+((Map<Object, Object>)activity).get("name"));
                     scaryList.add(Integer.parseInt(""+((Map<Object, Object>)activity).get("scariness")));
-                    //Log.d("ProfileUserActivity", "Activity name: "+((Map<Object, Object>)activity).get("name"));
+                    nContributorsList.add(Integer.parseInt(""+((Map<Object, Object>)activity).get("nContributors")));
+
+                    //Log.d("ProfileUserActivity", "Activity title: "+((Map<Object, Object>)activity).get("name"));
                     //Log.d("ProfileUserActivity", "Activity scariness: "+((Map<Object, Object>)activity).get("scariness"));
                 }
                 arrayAdapter.notifyDataSetChanged();
@@ -157,6 +168,30 @@ public class ProfileUserActivity extends Activity {
             // Push personal threshold back to firebase when done with user data
             Map<String, Object> newThreshold = new HashMap<String, Object>();
             newThreshold.put("threshold", threshold);
+
+            for(int i=0; i <nameList.size(); i++){
+                // scariness should go down or up if you say it should
+                if((swipeResults.get(i) == 1 && threshold < scaryList.get(i)) ||
+                        (swipeResults.get(i) == 0 && threshold > scaryList.get(i))){
+                    // Scariness only goes down because you calculated the new threshold and your new threshold is relatively low
+                    // likewise scariness only goes up because you calculated the new threshold and your new threshold is relatively high
+                    // We simply add the new threshold value to the running average and then divide by the new total number of contributors
+                    // ex: "how would you, a person, add a number to a number that was already averaged out by 9 people? (numberToAdd + runningTotal*9) / (9+1)
+
+                    int nC = nContributorsList.get(i);
+                    // This next line is just updating the scariness. Could have also done scaryList.set(i, nC*....)
+                    int newScariness = (nC*scaryList.get(i) + threshold) / (nC + 1);
+                    // This next line increases the nContributors for that activity by one in firebase
+                    HashMap<String, Object> newEntry = new HashMap<String, Object>();
+                    newEntry.put("scariness", newScariness);
+                    newEntry.put("nContributors", nC + 1);
+
+                    Log.d("New Scariness", ""+newScariness);
+                    // child has to be something else like activity1 or activity2 etc as in firebase
+                    firebaseRef.child("activities").child(activityNameList.get(i)).updateChildren(newEntry);
+                }
+            }
+
             Log.d("ProfileUserActivity", "User: "+username+" has threshold: "+threshold);
             firebaseRef.child("users").child(username).updateChildren(newThreshold);
 
